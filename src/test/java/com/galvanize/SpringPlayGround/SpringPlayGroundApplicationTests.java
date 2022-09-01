e9e0d5d5682974b30b5c4078e5422753d05319a0
kane-droid <kaneheath96@gmail.com>
2022-09-01 10:54:32 -0500

    Initial commit

---
+++ src/test/java/com/galvanize/SpringPlayGround/SpringPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.SpringPlayGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
